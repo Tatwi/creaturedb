6669f004d33da95b7cddaf86716dd4b669d1e906
R. Bassett Jr <alsopeachy@gmail.com>
2019-05-01 13:42:22 -0400

    Special attack names on the creature data page now match the names displayed in the game

--- creaturepage.php
+++ creaturepage.php
@@ -17,6 +17,53 @@
 			return ucwords(strtolower(str_replace("_", " ", $value)));
 		}
 		
+		function specialAttackIs($value){
+			if ($value == ""){
+				return "None";
+			} else if ($value == ""){
+			
+			} else if ($value == "creatureareacombo"){
+				return "Area Combo";
+			} else if ($value == "blindattack"){
+				return "Blinding Strike";
+			} else if ($value == "creatureareaattack"){
+				return "Creature Area Attack";
+			} else if ($value == "posturedownattack"){
+				return "Crippling Strike";
+			} else if ($value == "dizzyattack"){
+				return "Dizzy Strike";
+			} else if ($value == "creatureareaknockdown"){
+				return "Force Strike";
+			} else if ($value == "intimidationattack"){
+				return "Intimidation";
+			} else if ($value == "knockdownattack"){
+				return "Knockdown";
+			} else if ($value == "mediumdisease"){
+				return "Medium Disease";
+			} else if ($value == "mediumpoison"){
+				return "Medium Poison";
+			} else if ($value == "milddisease"){
+				return "Mild Disease";
+			} else if ($value == "mildpoison"){
+				return "Mild Poison";
+			} else if ($value == "creatureareableeding"){
+				return "Open Wounds";
+			} else if ($value == "creatureareadisease"){
+				return "Plague Strike";
+			} else if ($value == "creatureareapoison"){
+				return "Poison Spray";
+			} else if ($value == "strongdisease"){
+				return "Strong Disease";
+			} else if ($value == "strongpoison"){
+				return "Strong Poison";
+			} else if ($value == "stunattack"){
+				return "Stunning Strike";
+			} 
+			
+			// There are a few cases the value will be NA
+			return $value;
+		}
+		
 		$argument1 = $_GET['argument1'];
 		
 		// Creature Image
@@ -72,85 +119,76 @@
 			
 			// Combat Information
 			echo "<div class='crcombat'><div class='crcombattitle'>Combat Information</div>";
-		
-		$attack1 = $row["Attack_1"];
-		if ($attack1 == ""){
-			$attack1 = "None";
-		}
-		echo "<b>Special Attack 1:</b> ". $attack1. "<br />";
-		
-		$attack2 = $row["Attack_2"];
-		if ($attack2 == ""){
-			$attack2 = "None";
-		}
-		echo "<b>Special Attack 2:</b> ". $attack2. "<br />";
-		
-		$attackranged = $row["Ranged_Attack"];
-		if ($attackranged == ""){
-			$attackranged = "No";
-		} else {
-			$attackranged = "Yes";
-		}
-		echo "<b>Has Ranged Attack:</b> ". $attackranged. "<br /><br />";
-		
-		echo "<b>Kinetic Resistance:</b> ". checkVulnerable($row["Kinetic"]). "<br />";
-		echo "<b>Energy Resistance:</b> ". checkVulnerable($row["Energy"]). "<br />";
-		echo "<b>Blast Resistance:</b> ". checkVulnerable($row["Blast"]). "<br />";
-		echo "<b>Heat Resistance:</b> ". checkVulnerable($row["Heat"]). "<br />";
-		echo "<b>Cold Resistance:</b> ". checkVulnerable($row["Cold"]). "<br />";
-		echo "<b>Electric Resistance:</b> ". checkVulnerable($row["Electric"]). "<br />";
-		echo "<b>Acid Resistance:</b> ". checkVulnerable($row["Acid"]). "<br />";
-		echo "<b>Stun Resistance:</b> ". checkVulnerable($row["Stun"]). "<br />";
-		
-		echo "<b>Lightsaber Resistance:</b> " . checkVulnerable($row["Lightsaber"]) . "</div>";
-		
-		// Biographical Data
-		echo "<div class='crbio'><div class='crbiotitle'>Biographical Data</div>";
-		echo "<b>Social Group:</b> ". makePretty($row["Social_Group"]). "<br />";
-		echo "<b>Diet:</b> ". makePretty($row["Diet"]). "<br />";
-		
-		if (strpos($row["PVP_Bitmask"], 'AGGRESSIVE') !== false) {
-			echo "<b>Aggressive:</b> Yes<br />";
-		} else {
-			echo "<b>Aggressive:</b> No<br />";
-		}
 
-		$deathblow = $row["PVP_Bitmask"];
-		if (strpos($row["Creature_Bitmask"], 'KILLER') !== false) {
-			echo "<b>Deathblows:</b> Yes<br />";
+			echo "<b>Special Attack 1:</b> ". specialAttackIs($row["Attack_1"]). "<br />";
+			echo "<b>Special Attack 2:</b> ". specialAttackIs($row["Attack_2"]). "<br />";
+			
+			$attackranged = $row["Ranged_Attack"];
+			if ($attackranged == ""){
+				$attackranged = "No";
+			} else {
+				$attackranged = "Yes";
+			}
+			echo "<b>Has Ranged Attack:</b> ". $attackranged. "<br /><br />";
+			
+			echo "<b>Kinetic Resistance:</b> ". checkVulnerable($row["Kinetic"]). "<br />";
+			echo "<b>Energy Resistance:</b> ". checkVulnerable($row["Energy"]). "<br />";
+			echo "<b>Blast Resistance:</b> ". checkVulnerable($row["Blast"]). "<br />";
+			echo "<b>Heat Resistance:</b> ". checkVulnerable($row["Heat"]). "<br />";
+			echo "<b>Cold Resistance:</b> ". checkVulnerable($row["Cold"]). "<br />";
+			echo "<b>Electric Resistance:</b> ". checkVulnerable($row["Electric"]). "<br />";
+			echo "<b>Acid Resistance:</b> ". checkVulnerable($row["Acid"]). "<br />";
+			echo "<b>Stun Resistance:</b> ". checkVulnerable($row["Stun"]). "<br />";
+			
+			echo "<b>Lightsaber Resistance:</b> " . checkVulnerable($row["Lightsaber"]) . "</div>";
+			
+			// Biographical Data
+			echo "<div class='crbio'><div class='crbiotitle'>Biographical Data</div>";
+			echo "<b>Social Group:</b> ". makePretty($row["Social_Group"]). "<br />";
+			echo "<b>Diet:</b> ". makePretty($row["Diet"]). "<br />";
+			
+			if (strpos($row["PVP_Bitmask"], 'AGGRESSIVE') !== false) {
+				echo "<b>Aggressive:</b> Yes<br />";
+			} else {
+				echo "<b>Aggressive:</b> No<br />";
+			}
+
+			$deathblow = $row["PVP_Bitmask"];
+			if (strpos($row["Creature_Bitmask"], 'KILLER') !== false) {
+				echo "<b>Deathblows:</b> Yes<br />";
+			} else {
+				echo "<b>Deathblows:</b> No<br />";
+			}
+			
+			if (strpos($row["Creature_Bitmask"], 'PACK') !== false) {
+				echo "<b>Assists Allies:</b> Yes<br />";
+			} else {
+				echo "<b>Assists Allies:</b> No<br />";
+			}
+			
+			if (strpos($row["Creature_Bitmask"], 'STALKER') !== false) {
+				echo "<b>Stalks Prey:</b> Yes<br /><br />";
+			} else {
+				echo "<b>Stalks Prey:</b> No<br /><br />";
+			}
+			
+			echo "<b>Bone:</b> ". str_replace("Bone", "", makePretty($row["Bone_Type"])). " ". number_format($row["Bone_Amount"])." <br />";
+			echo "<b>Hide:</b> ". str_replace("Hide", "", makePretty($row["Hide_Type"])). " ". number_format($row["Hide_Amount"])." <br />";
+			echo "<b>Meat:</b> ". str_replace("Meat", "", makePretty($row["Meat_Type"])). " ". number_format($row["Meat_Amount"])." <br />";
+			echo "<b>Milk:</b> ". str_replace("Milk", "", makePretty($row["Milk_Type"])). " ". number_format($row["Milk_Amount"])." <br /><br />";
+			
+			if ($row["Taming_Chance"] > 0) {
+				echo "<b>Tamable:</b> Yes<br />";
+			} else {
+				echo "<b>Tamable:</b> No<br />";
+			}
+			
+			echo "<b>Ferocity:</b> ". number_format($row["Ferocity"]). "<br />";
+			echo "<b>Mountable:</b> ". $row["Mount"]. "<br />";
+			echo "<b>Can Sample DNA:</b> ". $row["BE_Can_Sample"]. "<br />";
+			echo "<b>Bio-Engineer Craftable:</b> ". $row["BE_Craftable"]. "<br />";
 		} else {
-			echo "<b>Deathblows:</b> No<br />";
-		}
-		
-		if (strpos($row["Creature_Bitmask"], 'PACK') !== false) {
-			echo "<b>Assists Allies:</b> Yes<br />";
-		} else {
-			echo "<b>Assists Allies:</b> No<br />";
-		}
-		
-		if (strpos($row["Creature_Bitmask"], 'STALKER') !== false) {
-			echo "<b>Stalks Prey:</b> Yes<br /><br />";
-		} else {
-			echo "<b>Stalks Prey:</b> No<br /><br />";
-		}
-		
-		echo "<b>Bone:</b> ". str_replace("Bone", "", makePretty($row["Bone_Type"])). " ". number_format($row["Bone_Amount"])." <br />";
-		echo "<b>Hide:</b> ". str_replace("Hide", "", makePretty($row["Hide_Type"])). " ". number_format($row["Hide_Amount"])." <br />";
-		echo "<b>Meat:</b> ". str_replace("Meat", "", makePretty($row["Meat_Type"])). " ". number_format($row["Meat_Amount"])." <br />";
-		echo "<b>Milk:</b> ". str_replace("Milk", "", makePretty($row["Milk_Type"])). " ". number_format($row["Milk_Amount"])." <br /><br />";
-		
-		if ($row["Taming_Chance"] > 0) {
-			echo "<b>Tamable:</b> Yes<br />";
-		} else {
-			echo "<b>Tamable:</b> No<br />";
-		}
-		
-		echo "<b>Ferocity:</b> ". number_format($row["Ferocity"]). "<br />";
-		echo "<b>Mountable:</b> ". $row["Mount"]. "<br />";
-		echo "<b>Can Sample DNA:</b> ". $row["BE_Can_Sample"]. "<br />";
-		echo "<b>Bio-Engineer Craftable:</b> ". $row["BE_Craftable"]. "<br />";
-		} else {
-			echo "0 results";
+			echo "Unable to load creature data at this time.";
 		}
 		
 		$conn->close();
